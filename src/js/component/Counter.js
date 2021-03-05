18e5907731fbd06676774b56474dc3d80e7a1c63
menatassara <josemt-12-@hotmail.com>
2021-03-05 03:09:49 +0000

    completo 3 final

--- src/js/component/Counter.js
+++ src/js/component/Counter.js
@@ -9,37 +9,24 @@ const Counter = props => {
 				<div>
 					<i className="far fa-clock"></i>
 				</div>
-				<div>[props.dg6 % 10]</div>
-				<div>[props.dg5 % 10]</div>
-				<div>[props.dg4 % 10]</div>
-				<div>[props.dg3 % 10]</div>
-				<div>[props.dg2 % 10]</div>
-				<div>[props.dg1 % 10]</div>
+				<div>{props.dg6 % 10}</div>
+				<div>{props.dg5 % 10}</div>
+				<div>{props.dg4 % 10}</div>
+				<div>{props.dg3 % 10}</div>
+				<div>{props.dg2 % 10}</div>
+				<div>{props.dg1 % 10}</div>
 			</div>
 		</div>
 	);
 };
 
-let SecondsCounter = 0;
-setInterval(function() {
-	const digit1 = Math.floor(SecondsCounter);
-	const digit2 = Math.floor(SecondsCounter / 10);
-	const digit3 = Math.floor(SecondsCounter / 100);
-	const digit4 = Math.floor(SecondsCounter / 1000);
-	const digit5 = Math.floor(SecondsCounter / 10000);
-	const digit6 = Math.floor(SecondsCounter / 100000);
-	SecondsCounter++;
-	ReactDOM.render(
-		<Counter
-			dg1={digit1}
-			dg2={digit2}
-			dg3={digit3}
-			dg4={digit4}
-			dg5={digit5}
-			dg6={digit6}
-		/>,
-		document.querySelector("#app")
-	);
-}, 1000);
+Counter.propTypes = {
+	dg1: PropTypes.number,
+	dg2: PropTypes.number,
+	dg3: PropTypes.number,
+	dg4: PropTypes.number,
+	dg5: PropTypes.number,
+	dg6: PropTypes.number
+};
 
 export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,24 @@ import "../styles/index.scss";
 import Counter from "./component/Counter.js";
 
 //render your react application
-ReactDOM.render(<Counter />, document.querySelector("#app"));
+let SecondsCounter = 0;
+setInterval(function() {
+	const digit1 = Math.floor(SecondsCounter);
+	const digit2 = Math.floor(SecondsCounter / 10);
+	const digit3 = Math.floor(SecondsCounter / 100);
+	const digit4 = Math.floor(SecondsCounter / 1000);
+	const digit5 = Math.floor(SecondsCounter / 10000);
+	const digit6 = Math.floor(SecondsCounter / 100000);
+	SecondsCounter++;
+	ReactDOM.render(
+		<Counter
+			dg1={digit1}
+			dg2={digit2}
+			dg3={digit3}
+			dg4={digit4}
+			dg5={digit5}
+			dg6={digit6}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
